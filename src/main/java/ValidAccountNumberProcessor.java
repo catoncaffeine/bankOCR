8c5bef04d679ec19924aa997e2fbbfbe65e3dd00
Cat Guo <cat.itba@gmail.com>
2018-08-14 08:56:05 -0400

    Story 4: adding class to help identify possible valid account numbers

---
+++ src/main/java/ValidAccountNumberProcessor.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ValidAccountNumberProcessor {
+    public static ArrayList<String> getPossibleAccountNumbers(String accountNumber) {
+        ArrayList<String> possibleAccountNumbers = new ArrayList<>();
+        possibleAccountNumbers.add(accountNumber);
+        return possibleAccountNumbers;
+    }
+}
---
+++ src/test/java/ValidAccountNumberProcessor.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class ValidAccountNumberProcessor {
+
+    @Test
+    public void testGetPossibleAccountNumbers_onePossible() {
+
+    }
+
+    @Test
+    public void testGetPossibleAccountNumbers_multiplePossible() {
+
+    }
+
+    @Test
+    public void testGetPossibleAccountNumbers_nonePossible() {
+
+    }
+}
